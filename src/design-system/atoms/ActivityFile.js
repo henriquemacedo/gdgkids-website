4dc5fb5c1e6b2c7cdf8fbc4f2262de38a6c200de
Henrique Macedo <mail@henriquemacedo.pt>
2019-01-06 18:02:54 +0000

    Added activity info

--- src/design-system/atoms/ActivityFile.js
+++ src/design-system/atoms/ActivityFile.js
@@ -2,9 +2,64 @@ import React, { Component } from "react";
 import ReactMarkdown from "react-markdown";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import duration from "../../images/activity-duration.svg";
+import group from "../../images/activity-group.svg";
+import difficult from "../../images/activity-difficult.svg";
+import material from "../../images/activity-material.svg";
 
-const ActivityFile = styled.div`
-  min-height: 100%;
+const ActivityFile = styled.div``;
+
+const ActivityInfo = styled.div`
+  margin-bottom: 2vw;
+  border-radius: 20px;
+  background-color: var(--white, hsl(0, 0%, 0%));
+  padding: 2vw;
+  box-shadow: 0 0 50px rgba(0, 0, 0, 0.2);
+
+  ul {
+    margin: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0;
+    list-style: none;
+
+    li {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      align-items: center;
+      width: 100%;
+
+      img {
+        margin-right: 10px;
+        width: auto;
+        height: 50px;
+      }
+
+      > div {
+        line-height: 0.8rem;
+
+        span {
+          &:first-child {
+            display: block;
+            font-size: 0.5rem;
+            font-weight: var(--black, 900);
+            text-transform: uppercase;
+          }
+
+          &:last-child {
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+        }
+      }
+    }
+  }
+`;
+
+const File = styled.div`
   border-radius: 20px;
   background-color: var(--white, hsl(0, 0%, 0%));
   padding: 2vw;
@@ -31,7 +86,41 @@ export default class extends Component {
   render() {
     return (
       <ActivityFile>
-        <ReactMarkdown source={this.state.md} />
+        <ActivityInfo>
+          <ul>
+            <li>
+              <img src={duration} alt="" />
+              <div>
+                <span>Duration</span>
+                <span>{this.props.duration} min</span>
+              </div>
+            </li>
+            <li>
+              <img src={group} alt="" />
+              <div>
+                <span>Group</span>
+                <span>{this.props.group}</span>
+              </div>
+            </li>
+            <li>
+              <img src={difficult} alt="" />
+              <div>
+                <span>Difficult</span>
+                <span>{this.props.difficult}</span>
+              </div>
+            </li>
+            <li>
+              <img src={material} alt="" />
+              <div>
+                <span>Material</span>
+                <span>{this.props.material}</span>
+              </div>
+            </li>
+          </ul>
+        </ActivityInfo>
+        <File>
+          <ReactMarkdown source={this.state.md} />
+        </File>
       </ActivityFile>
     );
   }
--- src/pages/ActLegoMindstorms.js
+++ src/pages/ActLegoMindstorms.js
@@ -4,6 +4,14 @@ import actLegoMindstorms from "../utils/actLegoMindstorms.md";
 
 export default class extends Component {
   render() {
-    return <ActivityFile file={actLegoMindstorms} />;
+    return (
+      <ActivityFile
+        duration="60-120"
+        group="1-10"
+        difficult="Medium"
+        material="EV3 robot, computer or tablet"
+        file={actLegoMindstorms}
+      />
+    );
   }
 }
